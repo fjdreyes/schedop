5ff8d9a2671c66fcee32a8a77f331d42820e630f
Franz Julian Delos Reyes <fjdreyes@gmail.com>
2025-05-01 16:42:48 +0800

    Add TailwindFormatter

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Schedop.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tailwind_formatter, "~> 0.4.2", only: [:dev, :test], runtime: false}
     ]
   end
 
